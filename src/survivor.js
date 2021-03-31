7e49cbfa636964ebadbd4984a4db12b0679717c5
Blake Runyon <blakerunyon09@gmail.com>
2021-03-31 17:25:28 -0600

    Added Profile Page

--- src/survivor.js
+++ src/survivor.js
@@ -5,34 +5,131 @@ const survivorID = 'http://localhost:8080/api/survivors?id='
 fetch(survivorID + id)
 .then(res => res.json())
 .then(survivor => {
-  const section = document.querySelector('#survivor_bio')
-  section.innerHTML = `
-    Name: ${survivor[0].name}<br>
-    Season Number: ${survivor[0].season_num}<br>
-    Season: ${survivor[0].season}<br>
-    Age: ${survivor[0].age}<br>
-    Gender: ${survivor[0].gender}<br>
-    State: ${survivor[0].state}<br>
-    Style: ${survivor[0].style}<br>
-    Days Played: ${survivor[0].days_played}<br>
-    Exit Condition: ${survivor[0].exit_condition}<br>
-    Final Vote: ${survivor[0].final_vote}<br>
-    First Vote Accuracy: ${survivor[0].first_vote_accuracy}<br>
-    Idols Found: ${survivor[0].idols_found}<br>
-    Individual Immunity: ${survivor[0].ind_immunity}<br>
-    Idols Played: ${survivor[0].idols_played}<br>
-    Individual Wins: ${survivor[0].ind_wins}<br>
-    Jury Member: ${survivor[0].jury_member}<br>
-    Place: ${survivor[0].place}<br>
-    Race: ${survivor[0].race}<br>
-    Sexuality: ${survivor[0].sexuality}<br>
-    Survivor Score: ${survivor[0].survivor_score}<br>
-    Tribe: ${survivor[0].tribe}<br>
-    Tribe Wins: ${survivor[0].tribe_wins}<br>
-    Vote Accuracy: ${survivor[0].vote_accuracy}<br>
-    Votes Against: ${survivor[0].votes_against}<br>
-    ID: ${survivor[0].id}<br>
+    // ATTACH NAME TO HERO BANNER
+    document.querySelector('#hero').firstElementChild.textContent = survivor[0].name
+    
+    // CREATE PROFILE BODY
+    survivorBoby = `
+      <section class="flex flex-col md:flex-row justify-center mt-4 mb-12 w-full">
+        <div class="text-2xl text-green-dark">
+          <img class="rounded-lg" src="${survivor[0].image}">
+          <h1 class="font-bold text-4xl my-6">${survivor[0].name}</h1>
+          <div class="text-xl text-green-dark">
+            <p class="font-bold text-2xl my-8">Demographics:</p>
+              <p class="ml-4 mt-2">Season: ${survivor[0].season} ${survivor[0].season_num}</p>
+              <p class="ml-4 mt-2">State: ${survivor[0].state}</p>
+              <p class="ml-4 mt-2">Style: ${survivor[0].style}</p>
+          </div>
+          <div class="text-xl text-green-dark">
+              <p class="font-bold text-2xl my-8">Demographics:</p>
+                <p class="ml-4 mt-2">State: ${survivor[0].state}</p>
+                <p class="ml-4 mt-2">Style: ${survivor[0].style}</p>
+                <p class="ml-4 mt-2">Race: ${survivor[0].race}</p>
+                <p class="ml-4 mt-2">Sexuality: ${survivor[0].sexuality}</p>
+          </div>
+        </div>
+        <div class="md:ml-20 text-xl text-green-dark">
+          <p class="font-bold text-2xl mb-8">Demographics:</p>
+            <p class="ml-4 mt-2">State: ${survivor[0].state}</p>
+            <p class="ml-4 mt-2">Style: ${survivor[0].style}</p>
+            <p class="ml-4 mt-2">Race: ${survivor[0].race}</p>
+            <p class="ml-4 mt-2">Sexuality: ${survivor[0].sexuality}</p>
+          <p class="font-bold text-2xl my-8">Tribe Stats:</p>
+            <p class="ml-4 mt-2">Tribe: ${survivor[0].tribe}</p>
+            <p class="ml-4 mt-2">Tribe Wins: ${survivor[0].tribe_wins}</p>
+          <p class="font-bold text-2xl my-8">Attributes:</p>
+            <p class="ml-4 mt-2">Days Played: ${survivor[0].days_played}</p>
+            <p class="ml-4 mt-2">Exit Condition: ${survivor[0].exit_condition}</p>
+            <p class="ml-4 mt-2">Idols Found: ${survivor[0].idols_found}</p>
+            <p class="ml-4 mt-2">Individual Immunity: ${survivor[0].ind_immunity}</p>
+            <p class="ml-4 mt-2">Idols Played: ${survivor[0].idols_played}</p>
+            <p class="ml-4 mt-2">Individual Wins: ${survivor[0].ind_wins}</p>
+            <p class="ml-4 mt-2">Place: ${survivor[0].place}</p>
+            <p class="ml-4 mt-2">Survivor Score: ${survivor[0].survivor_score}</p>
+          <p class="font-bold text-2xl my-8">Voting Record:</p>
+            <p class="ml-4 mt-2">First Vote Accuracy: ${survivor[0].first_vote_accuracy ? "Yes" : "No"}</p>
+            <p class="ml-4 mt-2">Vote Accuracy: ${survivor[0].vote_accuracy}</p>
+            <p class="ml-4 mt-2">Votes Against: ${survivor[0].votes_against}</p>
+            <p class="ml-4 mt-2">Jury Member: ${survivor[0].jury_member ? "Yes" : "No"}</p>
+            <p class="ml-4 mt-2">Final Vote: ${survivor[0].final_vote}</p>
+        </div>
+      </section>
+      <section class="flex flex-col flex-wrap md:flex-row justify-center mt-12 w-full">
+        <h6 class="w-full text-center text-blue-dark font-bold text-4xl my-6">Similar Survivors</h6>
+        <div id="related_survivors" class="flex flex-col md:flex-row"></div>
+      </section>
     `
-    console.log(survivor[0])
+    // GRAB AND APPEND BODY
+    const body = document.querySelector('#survivor_bio')
+    body.innerHTML = survivorBoby
+
+
+    const relSurvivors = document.querySelector('#related_survivors')
+
+    fetch(`http://localhost:8080/api/survivors?style=${survivor[0].style}`)
+      .then(res => res.json())
+      .then(relatedSurvivors => {
+        for(let i = 0; i < 3; i++){
+          let randNum = (Math.floor(Math.random() * relatedSurvivors.length))
+          let ranSurv = relatedSurvivors[randNum]
+
+          const survivorCard = createNode('div', 'card')
+          survivorCard.id = `surv_${survivor.id}`
+          createLeftDiv(ranSurv, survivorCard)
+          createRightDiv(ranSurv, survivorCard)
+      
+          relSurvivors.append(survivorCard)
+      }})
 })
+
+// CREATES NODE & CLASS
+const createNode = (element, className) => {
+  const el = document.createElement(element)
+  el.classList.add(className)
+  return el
+}      
+
+// CREATE LEFT DIV - CARD
+const createLeftDiv = (survivor, survivorCard) => {
+  const leftDiv = createNode('div', 'left-card-col')
+
+  const survivorImg = document.createElement('img')
+  survivorImg.src = survivor.image
+
+  const survivorName = createNode('h3')
+  survivorName.textContent = survivor.name
+
+  const survivorSeason = createNode('p')
+  survivorSeason.textContent = survivor.season
+
+  leftDiv.append(survivorImg, survivorName, survivorSeason)
+  survivorCard.append(leftDiv)
+}
+
+// CREATE RIGHT DIV - CARD
+const createRightDiv = (survivor, survivorCard) => {
+  const rightDiv = document.createElement('div')
+  rightDiv.classList.add('right-card-col')
+
+  const topDiv = createNode('div', 'right-card-col-top')
+  const topDivInner = createNode('div')
+  topDivInner.innerHTML = `
+    <p><strong>Season:</strong> ${survivor.season_num}</p>
+    <p><strong>Accuracy:</strong> ${survivor.vote_accuracy}</p>
+    <p><strong>Immunity:</strong> ${survivor.ind_immunity + survivor.idols_found}</p>
+    <p><strong>Place:</strong> ${survivor.place}</p>
+    <p><strong>Type:</strong> ${survivor.style}</p>
+    <p><strong>Score:</strong> ${survivor.survivor_score}</p>
+  `
+  topDiv.append(topDivInner)
+
+  const bottomDiv = createNode('div', 'right-card-col-bottom')
+  const viewButton = createNode('button', 'btn')
+  viewButton.innerHTML = `<a href="/survivor.html?id=${survivor.id}">VIEW</a>`
+  bottomDiv.append(viewButton)
+
+  rightDiv.append(topDiv, bottomDiv)
+
+  survivorCard.append(rightDiv)
+}
 
--- src/survivors.js
+++ src/survivors.js
@@ -1,9 +1,15 @@
 // Globals
 const survivorsAPI = 'http://localhost:8080/api/survivors'
 let globalSort = ''
+let tribesArray = [[],[]]
+let idRange = {minID: 0, length: 0}
+
+// 
+// FETCH, FILTER, AND SORT CARDS
+// 
 
 // FETCH SURVIVORS ON LOAD
-fetchSurvivors(survivorsAPI)
+fetchSurvivors(survivorsAPI + '?season_num=1')
 
 // FILTER & SORT UPDATES
 const survivorFinderBar = document.querySelector('#finder_bar')
@@ -23,12 +29,6 @@ survivorFinderBar.addEventListener('change', (e) => {
   fetchSurvivors(query)
 })
 
-// BUILD QUERY OPTIONS
-function queryBuilder(id, queryParam){
-  const el = document.querySelector(`#${id}`)
-  return el.value ? `${queryParam}=${el.value}&` : ''
-}
-
 // FETCH SURVIVORS FUNCTION
 function fetchSurvivors(query){
   const survivorContainer = document.querySelector('#survivor_cards')
@@ -36,15 +36,165 @@ function fetchSurvivors(query){
 
   fetch(query)
   .then(res => res.json())
-  .then(survivors => survivors.forEach(survivor => {
-    const survivorCard = createNode('div', 'card')
-
-    createLeftDiv(survivor, survivorCard)
-    createRightDiv(survivor, survivorCard)
-
-    survivorContainer.append(survivorCard)
-  }))
+  .then(survivors => {
+    idRange.minID = survivors[0].id
+    idRange.length = survivors.length
+    survivors.forEach(survivor => {
+      const survivorCard = createNode('div', 'card')
+      survivorCard.id = `surv_${survivor.id}`
+      createLeftDiv(survivor, survivorCard)
+      createRightDiv(survivor, survivorCard)
+  
+      survivorContainer.append(survivorCard)
+    })
+  })
 }
+
+// BUILD QUERY OPTIONS
+function queryBuilder(id, queryParam){
+  const el = document.querySelector(`#${id}`)
+  return el.value ? `${queryParam}=${el.value}&` : ''
+}
+
+// 
+// CREATE TRIBES
+// 
+
+// ADD SURVIVOR TO HOME TRIBE
+const addSurvivorToTribe = (survivor, survivorCard) => {
+  if(tribesArray[0].length < 6){
+    tribesArray[0].push({id: survivor.id, name: survivor.name})
+    survivorCard.remove()
+    addAwaySurvivor()
+  }else{
+    alert("Your Tribe Is Full. You're Ready to Play!")
+  }
+
+}
+
+// ADD SURVIVOR TO AWAY TRIBE
+const addAwaySurvivor = () => {
+  let awaySurv = (Math.floor(Math.random() * idRange.length) + idRange.minID)
+  fetch(`${survivorsAPI}?id=${awaySurv}`)
+    .then(res => res.json())
+    .then(survivor => {
+      tribesArray[1].push({id: survivor[0].id, name: survivor[0].name})
+      let setCookie = JSON.stringify(tribesArray)
+      document.cookie = `tribes=${setCookie}`
+      updateTribesBar()
+    })
+
+  const survivorToRemoveID = document.querySelector(`#surv_${awaySurv}`)
+  survivorToRemoveID.remove()
+}
+
+// 
+// DOM MANIPULATIONS
+// 
+
+// UPDATE TRIBE BAR
+const updateTribesBar = () => {
+  const homeTribeBar = document.querySelector('#homeTribeBar')
+  const awayTribeBar = document.querySelector('#awayTribeBar')
+
+  homeTribeBar.firstElementChild.textContent = ''
+  awayTribeBar.firstElementChild.textContent = ''
+
+  createTribeListItem()
+}
+
+const createTribeListItem = () => {
+  for(i = 0; i < 6; i++){
+    if(tribesArray[0][i]){
+      homeTribeBar.firstElementChild.innerHTML +=
+        `<div class="flex justify-between text-lg my-2 p-2 bg-white-dark text-green-dark rounded-md font-bold uppercase" style="box-shadow: inset 0px 8px 15px rgba(0, 0, 0, 0.1);">
+          <div class="" style="font-size: 15px">${tribesArray[0][i].name}</div><i class="fas fa-times text-base" style="padding: 5px 5px 0 0;"></i>
+        </div>`
+    }else{
+      homeTribeBar.firstElementChild.innerHTML +=
+      `<div class="flex justify-between text-lg my-2 p-2 bg-white-light rounded-md mb-2"  style="box-shadow: inset 0px 8px 15px rgba(0, 0, 0, 0.2); height: 42px;">
+      </div>`
+    }
+  
+    if(tribesArray[1][i]){
+      awayTribeBar.firstElementChild.innerHTML +=
+      `<div class="flex justify-between text-lg my-2 p-2 bg-white-dark text-green-dark rounded-md font-bold uppercase" style="box-shadow: inset 0px 8px 15px rgba(0, 0, 0, 0.1);">
+        <div class="" style="font-size: 15px">${tribesArray[1][i].name}</div><i class="fas fa-times text-base" style="padding: 5px 5px 0 0;"></i>
+      </div>`
+    }else{
+      awayTribeBar.firstElementChild.innerHTML +=
+      `<div class="flex justify-between text-lg my-2 p-2 bg-white-light rounded-md mb-2"  style="box-shadow: inset 0px 8px 15px rgba(0, 0, 0, 0.2); height: 42px;">
+      </div>`
+    }
+  }
+
+  awayTribeBar.firstElementChild.innerHTML += `
+  <div class="m-auto pb-4" style="font-size: 8px; width: fit-content">
+  </div>
+  `
+
+  homeTribeBar.firstElementChild.innerHTML += `
+  <div class="m-auto pb-4" style="font-size: 8px; width: fit-content">
+  </div>
+  `
+
+  for(i = 0; i < 6; i++){
+    if(tribesArray[0][i]){
+      homeTribeBar.firstElementChild.lastElementChild.innerHTML +=
+        `<i class="fas fa-circle text-xs pr-1 text-blue-light"></i>`
+    }else{
+      homeTribeBar.firstElementChild.lastElementChild.innerHTML +=
+      `<i class="far fa-circle text-xs pr-1 text-blue-light"></i>`
+    }
+  
+    if(tribesArray[1][i]){
+      awayTribeBar.firstElementChild.lastElementChild.innerHTML +=
+      `<i class="fas fa-circle text-xs pr-1 text-blue-light"></i>`
+    }else{
+      awayTribeBar.firstElementChild.lastElementChild.innerHTML +=
+      `<i class="far fa-circle text-xs pr-1 text-blue-light"></i>`
+    }
+  }
+
+  if(tribesArray[0][5]){
+    const tribeMenu = document.querySelector('aside')
+    button = createNode('button', 'btn')
+    button.innerHTML = '<a href="/play">Play</a>'
+    tribeMenu.append(button)
+  }
+}
+
+// SHOW/HIDE TRIBE TABS
+const tribesMenu = document.querySelector('#tribesMenu')
+tribesMenu.addEventListener('click', (e) => {
+  const homeTribeBar = document.querySelector('#homeTribeBar')
+  const awayTribeBar = document.querySelector('#awayTribeBar')
+
+  if(e.target.id === 'homeTribeTab'){
+    if(homeTribeBar.classList.contains('hidden')){
+      homeTribeBar.classList.remove('hidden')
+      awayTribeBar.classList.add('hidden')
+      e.target.style.boxShadow = "unset"
+      e.target.nextElementSibling.style.boxShadow = "inset 5px 0 5px -6px #000, inset 0px -9px 5px -9px #000"
+    }
+  }
+
+  if(e.target.id === 'awayTribeTab'){
+    if(awayTribeBar.classList.contains('hidden')){
+      awayTribeBar.classList.remove('hidden')
+      homeTribeBar.classList.add('hidden')
+      e.target.style.boxShadow = "unset"
+      e.target.previousElementSibling.style.boxShadow = "inset -6px 0 5px -6px rgba(0,0,0,0.75), inset 0px -9px 5px -9px rgba(0,0,0,0.75)"
+    }
+  }
+
+  if(e.target.classList.contains('fas')){
+    e.target.parentNode.previousElementSibling.classList.toggle('hidden')
+    e.target.classList.toggle('fa-angle-up')
+    e.target.classList.toggle('fa-angle-down')
+  }
+})
+
 
 // HIDE FILTERS
 const filterAside = document.querySelector('#finder_bar')
@@ -94,6 +244,7 @@ const createRightDiv = (survivor, survivorCard) => {
   viewButton.innerHTML = `<a href="/survivor.html?id=${survivor.id}">VIEW</a>`
   const addButton = createNode('button', 'btn')
   addButton.textContent = 'ADD'
+  addButton.addEventListener('click', () => {addSurvivorToTribe(survivor, survivorCard)})
   bottomDiv.append(viewButton, addButton)
 
   rightDiv.append(topDiv, bottomDiv)
